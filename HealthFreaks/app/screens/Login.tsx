0a4bddfbd7ccdf8f00ffa35ff5e1bf7d33387949
Isaac Munoz <isaac.munoz0808@gmail.com>
2023-10-08 17:00:15 -0700

    Small tweaks to login page UI

--- HealthFreaks/app/screens/Login.tsx
+++ HealthFreaks/app/screens/Login.tsx
@@ -1,53 +1,63 @@
-import {View, Text, StyleSheet, Button, TextInput, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
-import React, { useState} from 'react';
+import { View, Text, StyleSheet, Button, TextInput, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
 import { FIREBASE_AUTH } from '../../FireBaseConfig';
 import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, SetPassword] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState('');         //email text entry current state
+    const [password, SetPassword] = useState('');   //password text entry current state
+    const [loading, setLoading] = useState(false);  //loading state
+    const [hidePassword, setHidePassword] = useState(true); //password entry visibility state
     const auth = FIREBASE_AUTH;
 
 
-const signIn = async () => {
-    setLoading(true);
-    try{
-        const response = await signInWithEmailAndPassword(auth, email, password);
-        console.log(response);
-    }   catch (error: any){
-        console.log(error);
-        alert('Sign in failed: ' + error.message);
-    }   finally {
-        setLoading(false);
-    }
-};
+    const signIn = async () => {
+        setLoading(true);
+        try {
+            const response = await signInWithEmailAndPassword(auth, email, password);
+            console.log(response);
+        } catch (error: any) {
+            console.log(error);
+            alert('Sign in failed: ' + error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-const signUp = async () => {
-    setLoading(true);
-    try{
-        const response = await createUserWithEmailAndPassword(auth, email, password);
-        alert('Check your emails!');
-        console.log(response);
-    }   catch (error: any) {
-        console.log(error);
-        alert('Sign in failed: ' + error.message);
-    }   finally {
-        setLoading(false);
-    } 
-};
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            alert('Check your emails!');
+            console.log(response);
+        } catch (error: any) {
+            console.log(error);
+            alert('Sign in failed: ' + error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <View style={styles.container}>
             <KeyboardAvoidingView behavior='padding'>
-            <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
-            <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="Password" autoCapitalize='none' onChangeText={(text) => SetPassword(text)}></TextInput>
-        { loading ? <ActivityIndicator size="large" color="#0000ff"/>
-        : <>
-        <Button title="Login" onPress={signIn}/>
-        <Button title="Create Account" onPress={signUp}/>
-        </>}
-        </KeyboardAvoidingView>
+                <Text style={styles.title}>Health Freaks</Text>
+                <TextInput value={email} style={styles.input} placeholder="Email" placeholderTextColor='#999' autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
+                <View style={[styles.input, styles.passwordInput]}>
+                    <TextInput secureTextEntry={hidePassword} value={password} style={{ flex: 1, paddingLeft: 10 }} placeholder="Password" placeholderTextColor='#999' autoCapitalize='none' onChangeText={(text) => SetPassword(text)}></TextInput>
+                    <Ionicons name={hidePassword ? 'eye-off' : 'eye'} size={32} style={{ alignSelf: 'center', padding: 4 }} onPress={() => { setHidePassword(!hidePassword) }} />
+                </View>
+                {loading ? <ActivityIndicator size="large" color="#0000ff" />
+                    : <>
+                        <View style={{marginTop:32}}>
+                            <Button title="Login" onPress={signIn} />
+                        </View>
+                        <View style={{marginTop:8}}>
+                            <Button title="Create Account" onPress={signUp} />
+                        </View>
+                    </>}
+            </KeyboardAvoidingView>
         </View>
     );
 };
@@ -58,7 +68,7 @@ const styles = StyleSheet.create({
     container: {
         marginHorizontal: 20,
         flex: 1,
-        justifyContent:'center',
+        justifyContent: 'center',
     },
     input: {
         marginVertical: 4,
@@ -66,7 +76,16 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 4,
         padding: 10,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+    },
+    passwordInput: {
+        flexDirection: 'row',
+        padding: 0,
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+        marginBottom: 32,
     }
 });
 
