db406fce8f9eca2ead36260101cc381742842b22
Isaac Munoz <isaac.munoz0808@gmail.com>
2023-10-17 12:31:19 -0700

    Work towards enabling step counter

--- HealthFreaks/app/screens/Dashboard.tsx
+++ HealthFreaks/app/screens/Dashboard.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+const useHealthData = () => {
+    const [steps, setSteps] = useState(0);
+    const [calories, setCalories] = useState(0);
+    const [distance, setDistance] = useState(0);
+
+    // HealthKit implementation
+    return { steps, calories, distance};
+};
+
 export default function Dashboard() {
+    const {steps, calories, distance} = useHealthData();
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>{'\t\t\t'}Goals{'\n\t'} Steps{'\t\t\t\t'} Calories
-                {'\n\t'} 10782 {'\t\t\t\t'}1992
-            </Text>
+            <View style={styles.bubble}>
+                <Text style={{ fontSize: 24, alignSelf: 'center' }}>
+                    Goal
+                </Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={[styles.container, { alignItems: 'center' }]}>
+                        <Text style={{ fontSize: 20 }}>Steps</Text>
+                        <Text style={{ fontSize: 20 }}>{steps}</Text>
+                    </View>
+                    <View style={[styles.container, { alignItems: 'center' }]}>
+                        <Text style={{ fontSize: 20 }}>Calories</Text>
+                        <Text style={{ fontSize: 20 }}>{calories}</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
@@ -14,17 +38,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
-        paddingTop: 0.1,
         backgroundColor: '#ecf0f1',
         padding: 8,
     },
-    text: {
-        fontSize: 25,
-        height: 200,
-        width: '100%',
-        backgroundColor: 'purple',
-        borderColor: '#000',
+    bubble: {
+        margin: 20,
         borderWidth: 1,
-        borderRadius: 4
+        borderRadius: 4,
+        flexDirection: 'column',
     }
 });
