ef23a6d87cbe870c951fcdc9af55395ef1418ef9
aut6ot <99927551+Aut6ot@users.noreply.github.com>
2023-11-21 13:38:55 -0800

    Update App.tsx

--- HealthFreaks/App.tsx
+++ HealthFreaks/App.tsx
@@ -5,6 +5,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FireBaseConfig';
+import { Text, View, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+
 
 
 /* [Individual pages for app] */
@@ -15,22 +19,57 @@ import BMICalc from './app/screens/BMICalc';
 import Timer from './app/screens/Timer';
 import Settings from './app/screens/Settings';
 import Profile from './app/screens/Profile';
+import { transform } from 'typescript';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Dashboard' component={Dashboard} />
-      <Tab.Screen name='Graphs' component={Graphs} />
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Dashboard') {
+          iconName = focused
+            ? 'globe'
+            : 'globe-outline';
+        } 
+        else if (route.name === 'Graphs') {
+          iconName = focused ? 'analytics' : 'analytics-outline';
+        }
+        else if (route.name === 'BMICalc') {
+          iconName = focused ? 'calculator' : 'calculator-outline';
+        }
+        else if (route.name === 'Timer') {
+          iconName = focused ? 'alarm' : 'alarm-outline';
+        }
+        else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'deeppink',
+      tabBarInactiveTintColor: 'cyan',
+      tabBarStyle: { position: 'absolute', borderTopWidth:0 },
+      tabBarBackground: () => (
+        <BlurView tint="dark" intensity={100} style={StyleSheet.absoluteFill} />
+      ),
+
+    })}
+  >
+      <Tab.Screen name='Dashboard' component={Dashboard} options={{ headerShown: false }} />
+      <Tab.Screen name='Graphs' component={Graphs} options={{ headerShown: false }} />
       <Tab.Screen name='BMICalc' component={BMICalc} options={{ headerShown: false }} />
-      <Tab.Screen name='Timer' component={Timer} />
-      <Tab.Screen name='Settings' component={Settings} />
+      <Tab.Screen name='Timer' component={Timer} options={{ headerShown: false }} />
+      <Tab.Screen name='Settings' component={Settings} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 };
-
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -43,11 +82,27 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        {user ? <Stack.Screen name='Home' component={Main} options={{ headerShown: false }} /> : <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />}
-        <Stack.Screen name='Profile Settings' component={Profile} />
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerTintColor: 'red', headerBackground: () => (<BlurView tint="dark" intensity={100} style={StyleSheet.absoluteFill} />
+            ),}}>
+        {user ? (
+        <Stack.Screen name='Home' component={Main} options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+      )}
+      <Stack.Screen
+          name='Profile Settings'
+          component={Profile}
+          options={{
+            title: 'Profile Settings',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
