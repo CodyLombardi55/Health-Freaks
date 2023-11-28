e189f6598908206f5c9003cad3a0b218b7d28012
Isaac Munoz <isaac.munoz0808@gmail.com>
2023-11-28 14:02:21 -0800

    Trying to fix android permissions

--- HealthFreaks/app/screens/Dashboard.tsx
+++ HealthFreaks/app/screens/Dashboard.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Platform, ImageBackground, Pressable } from "re
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as Font from 'expo-font';
+import googleFit, { Scopes } from "react-native-google-fit";
 
 /* Needed to properly load our custom font */
 const customFonts = {
@@ -23,9 +24,28 @@ const Stack = createNativeStackNavigator();
 function Dashboard({ navigation }) {
     const [steps, setSteps] = useState(0);
     const [calories, setCalories] = useState(0);
-    
+
     useEffect(() => {
         loadCustomFonts();
+        const options = {
+            scopes: [
+                Scopes.FITNESS_ACTIVITY_READ,
+                Scopes.FITNESS_ACTIVITY_WRITE,
+                Scopes.FITNESS_BODY_READ,
+                Scopes.FITNESS_BODY_WRITE,
+            ],
+        }
+        googleFit.authorize(options)
+            .then(authResult => {
+                if (authResult.success) {
+                    console.log("AUTH_SUCCESS");
+                } else {
+                    console.log("AUTH_DENIED", authResult.message);
+                }
+            })
+            .catch(() => {
+                console.log("AUTH_ERROR");
+            })
     }, []);
 
     return (
